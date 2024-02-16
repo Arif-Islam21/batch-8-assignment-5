d76bda5afbf691ea2d6f44a8c6071b06e8e21e7f
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-16 14:30:51 +0600

    title container added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // FIND THE VALUE WHEN CARD IS CLICKED
+const cardElement = document.querySelectorAll(".card");
+
+for (const card of cardElement) {
+  const cardH3 = card.querySelector("h3");
+  const cardTitle = cardH3.innerText;
+
+  const displaytitle = document.getElementById("title-container");
+  const p = document.createElement("p");
+  p.innerText = cardTitle;
+
+  card.addEventListener("click", function (e) {
+    displaytitle.appendChild(p);
+    console.log("some one clicked me");
+  });
+}
