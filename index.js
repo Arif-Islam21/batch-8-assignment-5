922633d496da811917ab1c403e1c3ea57f9aa115
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-16 14:35:16 +0600

    card number added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // FIND THE VALUE WHEN CARD IS CLICKED
 const cardElement = document.querySelectorAll(".card");
+let cardNumber = 1;
 
 for (const card of cardElement) {
   const cardH3 = card.querySelector("h3");
@@ -7,10 +8,11 @@ for (const card of cardElement) {
 
   const displaytitle = document.getElementById("title-container");
   const p = document.createElement("p");
-  p.innerText = cardTitle;
 
   card.addEventListener("click", function (e) {
     displaytitle.appendChild(p);
-    console.log("some one clicked me");
+    p.innerText = cardNumber + "." + cardTitle;
+    cardNumber++;
+    // console.log("some one clicked me");
   });
 }
