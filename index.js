6e2d0fb206c1ad404238babc1ee149355abf085e
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-16 14:44:24 +0600

    total price text done

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // FIND THE VALUE WHEN CARD IS CLICKED
 const cardElement = document.querySelectorAll(".card");
+const totalPriceElement = document.getElementById("totalPrice");
 let cardNumber = 1;
+let totalPrice = 0;
 
 for (const card of cardElement) {
   const cardH3 = card.querySelector("h3");
   const cardTitle = cardH3.innerText;
+
+  const cardPriceText = card.querySelector("span").innerText;
+  const splitCard = cardPriceText.split(" ");
+  const cardValue = parseFloat(splitCard[1]);
 
   const displaytitle = document.getElementById("title-container");
   const p = document.createElement("p");
@@ -14,5 +20,7 @@ for (const card of cardElement) {
     p.innerText = cardNumber + "." + cardTitle;
     cardNumber++;
     // console.log("some one clicked me");
+    totalPrice += cardValue;
+    totalPriceElement.innerText = totalPrice;
   });
 }
